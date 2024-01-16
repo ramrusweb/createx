77b07bc3aa6466feaa963fff1b41cf7b630c5069
ramrusweb <rustamramazanovworkmail@gmail.com>
2024-01-16 03:52:14 +0300

    added: testimonials slider section

--- public/js/slick.slider.connection.js
+++ public/js/slick.slider.connection.js
@@ -1,10 +1,9 @@
 $(function() {
+  // team slider
   $('.team__slider').slick({
     arrows: false,
     slidesToShow: 4,
-    infinite: true,
-    draggable: false,
-    waitForAnimate: false
+    infinite: true
   })
   // prev slide
   $('.team__slider-prev').on('click', function(e) {
@@ -15,5 +14,23 @@ $(function() {
   $('.team__slider-next').on('click', function(e) {
     e.preventDefault()
     $('.team__slider').slick('slickNext')
+  })
+
+  //testimonials slider
+  $('.testimonials__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: $('.testimonials__dots')
+  })
+
+  // prev slide
+  $('.testimonials__prev').on('click', function(e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+  })
+  // next slide
+  $('.testimonials__next').on('click', function(e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
   })
 }) 
